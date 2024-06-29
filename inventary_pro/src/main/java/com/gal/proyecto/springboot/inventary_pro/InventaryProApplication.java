6d27449b182ee2d7563dd1d7ce4aff0f77c3a870
Galhrrera <juan.galeanoherrera@gmail.com>
2024-06-29 10:53:15 -0500

    Se creó el proyecto

---
+++ inventary_pro/src/main/java/com/gal/proyecto/springboot/inventary_pro/InventaryProApplication.java
@@ -0,0 +1,13 @@
+package com.gal.proyecto.springboot.inventary_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaryProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaryProApplication.class, args);
+	}
+
+}
---
+++ inventary_pro/src/test/java/com/gal/proyecto/springboot/inventary_pro/InventaryProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gal.proyecto.springboot.inventary_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaryProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
